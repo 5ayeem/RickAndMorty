f9ba40b84cbf6de27f8f972a123b7fe064aa0165
5ayeem <Sayeem.hu@gmail.com>
2025-09-19 17:28:07 +0100

    Adds project

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Sayeem Hussain on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
